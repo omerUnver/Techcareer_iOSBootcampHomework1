3945ae913352577581c0a210759e84b116a7fad4
omerUnver <muhammedomerunver@gmail.com>
2023-09-03 22:48:03 +0300

    Techcareer.net iOS Bootcamp Homework1

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,98 @@
+import UIKit
+//Homework1 Muhammed Ömer Ünver
+let sehir = "Ankara"
+let ulke = "Türkiye"
+var telefon = "+90 111 000 11 00"
+let postaKodu = "06010"
+var email = "muhammedomerunver@gmail.com"
+let meslek = "iOS Developer"
+var stokMiktari = 500
+var musteriAdi = "Kasım Adalan"
+var bakiye = 999.99
+let dogumGunu = "05/03/1999"
+var maas = 20000
+let medeniDurum = "Bekar"
+let urunYorum = "Techcareer.net eğitimim Kasım Adalan hocam ile çok keyifli geçiyor."
+let odemeTarihi = "01/01/2024"
+let odeme = 50
+let siparisAdeti = 200
+let arabaModeli = "Tiguan"
+let kitapAdi = "İnsan ne ile yaşar"
+let yayinlanmaTarihi = 1885
+let indirimMiktari = 10
+let odaSayisi = 22
+let enlem = 39.9035233
+let boylam = 32.5979563
+let urunAdi = "Cep Telefonu"
+let yemekFiyati = 156.90
+let marka = "Volkswagen"
+let muzikAdi = "Yaşar - Birtanem"
+let videoSuresi = 03.23
+let urunPuani = 4.75
+let resimAdi = "techcareer.png" // Kullanılması gereken yerde Assets'e yüklenmiş görseli UIImage(named: "resimAdi") olarak kullanabiliriz.
+let dosyaFormati = ".swift"
+let renk = "blue"
+var renkKodu = "red:153/255, green: 153/255, blue: 255/255" //Kullanılması gereken yerde UIColor(red: 153/255, green: 153/255, blue: 255/255, alpha: 1) olarak kullanabiliriz renk kodu UIColor içerisinde kullanmam gerekiyorsa red,green,blue olarak 3 tane daha değişken kullanmam gerekiyor.
+let telefonModeli = "iPhone - 14 Pro"
+let ekranBoyutu = 6.1
+let agirlik = 89
+var ulusalGun = "30 Ağustos - Zafer Bayramı"
+var tatilGunu = ulusalGun
+var rezervasyonTarihi = "05/03/2024"
+let sokakAdi = "Çelikpaşa Sokak."
+let otobüsHatti = "263 - Etlik - Kızılay Hattı"
+let kalanDakika = 10
+let takipKodu = "T23FMS15"
+let kuponSuresi = 5
+let kuponKodu = "MOU38EYLUL2023"
+let faturaAdresi = "Nakipoğlu Mah. Çelikpaşa Sok. 00/00 Konya/Türkiye"
+
+//Değişkenlerin Yazdırılması
+
+print("Şehir             : \(sehir)")
+print("Ülke              : \(ulke)")
+print("Telefon           : \(telefon)")
+print("Posta Kodu        : \(postaKodu)")
+print("Email             : \(email)")
+print("Meslek            : \(meslek)")
+print("Stok Miktarı      : \(stokMiktari) Adet")
+print("Müşteri Adı       : \(musteriAdi)")
+print("Bakiye            : \(bakiye) ₺")
+print("Doğum Günü        : \(dogumGunu)")
+print("Maaş              : \(maas) ₺")
+print("Medeni Durum      : \(medeniDurum)")
+print("Ürün Yorum        : \(urunYorum)")
+print("Ödeme Tarihi      : \(odemeTarihi)")
+print("Ödeme             : \(odeme) ₺")
+print("Sipariş Adeti     : \(siparisAdeti) Adet")
+print("Araba Modeli      : \(arabaModeli)")
+print("Kitap Adı         : \(kitapAdi)")
+print("Yayınlanma Tarihi : \(yayinlanmaTarihi)")
+print("İndirim Miktari   : %\(indirimMiktari)")
+print("Oda Sayısı        : \(odaSayisi)")
+print("Enlem             : \(enlem)")
+print("Boylam            : \(boylam)")
+print("Ürün Adı          : \(urunAdi)")
+print("Yemek Fiyatı      : \(yemekFiyati) ₺")
+print("Marka             : \(marka)")
+print("Müzik Adı         : \(muzikAdi)")
+print("Video Süresi      : \(videoSuresi) dk")
+print("Ürün Puanı        : \(urunPuani) ⭐️")
+print("Resim Adı         : \(resimAdi)")
+print("Dosya Formatı     : \(dosyaFormati)")
+print("Renk              : \(renk)")
+print("Renk Kodu         : \(renkKodu)")
+print("Telefon Modeli    : \(telefonModeli)")
+print("Ekran Boyutu      : \(ekranBoyutu) inç")
+print("Ağırlık           : \(agirlik) Kg")
+print("Ulusal Gün        : \(ulusalGun)")
+print("Tatil Günü        : \(tatilGunu)")
+print("Rezervasyon Tarihi: \(rezervasyonTarihi)")
+print("Sokak Adı         : \(sokakAdi)")
+print("Otobüs Hattı      : \(otobüsHatti)")
+print("Kalan Dakika      : \(kalanDakika) dk")
+print("Takip Kodu        : \(takipKodu)")
+print("Kupon Süresi      : \(kuponSuresi) Gün")
+print("Kupon Kodu        : \(kuponKodu)")
+print("Fatura Adresi     : \(faturaAdresi)")
+
